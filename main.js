32e323e20e496b5aacd60d0126ec46e4ca21d463
Michał Gajda <misiek.krk@gmail.com>
2022-12-15 19:20:47 +0100

    add weather api to js 1

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import './sass/main.scss'
 
 let cityForm = document.querySelector('.weather__form')
 let cityInput = document.querySelector('.weather__city')
+let APIURL = 'http://api.weatherapi.com/v1/current.json?key=4feda2f012924810b53171957221512&aqi=yes&q='
+let apiView = document.querySelector('.weather__data')
+
 
 cityForm.addEventListener('submit', (event) => {
     let city = cityInput.value
@@ -9,6 +12,18 @@ cityForm.addEventListener('submit', (event) => {
         cityInput.classList.add('weather__city--error')
     } else {
         cityInput.classList.remove('weather__city--error')
+        let APIURLWITHCITY = APIURL + city;
+        fetch(APIURLWITHCITY)
+            .then((response) => response.json())
+            .then((dataFromAPI) => {
+                //console.log(dataFromAPI.current.temp_c)
+                let view = ''
+                view += `<b>${dataFromAPI.current.temp_c}</b>`
+
+
+
+                apiView.innerHTML = view
+            })
     }
 
     event.preventDefault()
