024c965315965eb3f7a4f5da39d6a9f0d2ce4ee1
Michał Gajda <misiek.krk@gmail.com>
2022-12-16 13:50:20 +0100

    change api add weather__description

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ import './sass/main.scss'
 
 let cityForm = document.querySelector('.weather__form')
 let cityInput = document.querySelector('.weather__city')
-let APIURL = 'https://api.weatherapi.com/v1/current.json?key=4feda2f012924810b53171957221512&aqi=yes&q='
+let APIURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+let APIKEY = '?unitGroup=metric&key=4UKB3K9E52F28BLZG5FH5ETSG&include=fcst%2Calerts%2Ccurrent'
 let apiView = document.querySelector('.weather__data')
 let loader = document.querySelector('.weather__loader')
+
+
 
 
 cityForm.addEventListener('submit', (event) => {
@@ -13,7 +16,7 @@ cityForm.addEventListener('submit', (event) => {
         cityInput.classList.add('weather__city--error')
     } else {
         cityInput.classList.remove('weather__city--error')
-        let APIURLWITHCITY = APIURL + city;
+        let APIURLWITHCITY = APIURL + city + APIKEY;
         showLoader()
         fetch(APIURLWITHCITY)
             .then((response) => {
@@ -26,21 +29,22 @@ cityForm.addEventListener('submit', (event) => {
             .then((dataFromAPI) => {
                 hideLoader();
                 let view = ''
-                view += `<div class="weather__location">${dataFromAPI.location.name} - ${dataFromAPI.location.country}</div>`
+                view += `<div class="weather__location">${dataFromAPI.resolvedAddress}</div>`
                 view += `<div class="weather__info">`
                 //icon
-                view += `<div class="weather__icon"><img src="${dataFromAPI.current.condition.icon}" alt="${dataFromAPI.current.condition.text}"></div>`
+                view += `<div class="weather__icon">${dataFromAPI.currentConditions.icon}</div>`
                 view += `<div class="weather__temp">
-                            <span class="weather__num">${dataFromAPI.current.temp_c}</span>
+                            <span class="weather__num">${dataFromAPI.currentConditions.temp}</span>
                             <span class="weather__unit">&deg;C</span>
                         </div>`
                 view += `<div class="weather__desc">
-                    <p class="weather__text">The amount of rainfall: ${dataFromAPI.current.precip_mm} mm</p>
-                    <p class="weather__text">Humidity: ${dataFromAPI.current.humidity} %</p>
-                    <p class="weather__text">Wind: ${dataFromAPI.current.wind_kph} kph</p>
+                    <p class="weather__text">The amount of rainfall: ${dataFromAPI.currentConditions.precip} mm</p>
+                    <p class="weather__text">Humidity: ${dataFromAPI.currentConditions.humidity} %</p>
+                    <p class="weather__text">Wind: ${dataFromAPI.currentConditions.windspeed} kph</p>
                 </div>`
 
                 view += `</div >`
+                view += `<div class="weather__description">${dataFromAPI.description}</div`
                 apiView.innerHTML = view
             })
     }
